7c6128ff1c4640fdef30a8ffa635d9a5a8f3fdef
Johnjimy Som <johnjimysom@gmail.com>
2018-09-28 11:14:22 -0700

    updated pdf-d.py

--- pdf-d.py
+++ pdf-d.py
@@ -6,7 +6,7 @@ pdfReader = PyPDF2.PdfFileReader(open('encrypted.pdf', 'rb'))
 pdfReader.isEncrypted
 
 pdfReader.getPage(0)
-pdfReader.decrypt('rosebud')
+pdfReader.decrypt('rosebud')#not the right password but to test
 
 
 pageObj = pdfReader.getPage(0)
